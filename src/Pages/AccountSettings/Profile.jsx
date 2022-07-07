7052f75571fa1c416b0a3fa07d669e059d2101cd
Ikwuegbuenyi Augustus <mikwuegbuenyi@gmail.com>
2022-07-07 11:11:02 +0100

    commit

---
+++ src/Pages/AccountSettings/Profile.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import {useNavigate, NavLink} from "react-router-dom";
+import Navbar from '../../Components/navbar/Navbar';
+
+const Profile = () => {
+ 
+  return (
+    
+    <div className='ml-[200px] px-6'>
+  
+       
+    </div>
+  )
+}
+
+export default Profile
