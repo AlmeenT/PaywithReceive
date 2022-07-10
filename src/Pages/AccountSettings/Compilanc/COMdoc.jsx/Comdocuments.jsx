aed71a20f679428f125912343a9365321fbabc11
Ikwuegbuenyi Augustus <mikwuegbuenyi@gmail.com>
2022-07-10 20:24:03 +0100

    working

--- src/Pages/AccountSettings/Compilanc/COMdoc.jsx/Comdocuments.jsx
+++ src/Pages/AccountSettings/Compilanc/COMdoc.jsx/Comdocuments.jsx
@@ -1,8 +1,120 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import './comdoc.css'
 const Comdocuments = () => {
+ const [submitted, setSubmitted] = useState(false);
+  const [error, setError] = useState(false);
+  const [cemail, setCemail] = useState('');
+  const [check, setCheck] = useState(false);
+  const [scemail, setScemail] = useState('');
+   const [username, setUsername] = useState('');
+    const [user, setUser] = useState('');
+
+   const handleCemail = (e) => {
+    setCemail(e.target.value);
+    setSubmitted(false);
+  };
+
+    const handleScemail = (e) => {
+    setScemail(e.target.value);
+    setSubmitted(false);
+  };
+
+  const handleCheck = (e) =>{
+    setCheck(e.target.value);
+   setSubmitted(false);
+  
+   };
+   
+  
+  
+  const handleUser = (e) => {
+    setUser(e.target.value);
+    setSubmitted(false);
+  };
+   const handleUsername = (e) => {
+    setUsername(e.target.value);
+    setSubmitted(false);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if ( cemail === '' || check === false & scemail === '') {
+      setError(true);
+    }
+      else if (scemail === ''& check === true){
+         setSubmitted(true);
+          setError(false);
+      }
+    else {
+      setSubmitted(true);
+      setError(false);
+    }
+  };
+  
+
+const errorMessage = () => {
+    return (
+      <div
+        className="error"
+        style={{
+          display: error ? '' : 'none',
+        }}>
+        <h1>Please enter all the fields</h1>
+      </div>
+    );
+  };
+   const successMessage = () => {
+    return (
+      <div
+        className="success"
+        style={{
+          display: submitted ? '' : 'none',
+        }}>
+        <h1>User {cemail} successfully registered!!</h1>
+      </div>
+    );
+  };
   return (
-    <div>Comdocuments</div>
+    
+     <div className='Painn'>
+    <div className="mporm">
+     <h1>Documents</h1>
+  
+ 
+      {/* Calling to the methods */}
+      
+        {errorMessage()}
+        {successMessage()}
+   
+ 
+     <form className='style-form'>
+      <label className="label">General Email</label>
+        <input onChange={handleCemail} 
+          value={cemail} type="file" placeholder='Enter email address' className='docfile'/>
+
+           <label className="label">Support Email<span>*</span></label>
+        <input onChange={handleScemail}
+          value={scemail} type="email" placeholder='Enter support email'/>
+          <div>
+            <input onChange={handleCheck}
+          value={check} type="checkbox"/>
+          <label className="label">Use general email</label>
+         
+          </div>
+
+
+          <label className="label">Phone Number</label>
+        <input onChange={handleUsername} className="input"
+          value={username} type="text" placeholder='Username' />
+
+           <label className="label">Website(optional)</label>
+        <input onChange={handleUser} className="input"
+          value={user} type="file" placeholder='User ID' />
+          </form>
+          </div>
+           <button onClick={handleSubmit} className="patn" type="submit">
+          Save changes
+        </button>
+    </div>
   )
 }
 
--- src/Pages/AccountSettings/Compilanc/COMreg/Comregistration.jsx
+++ src/Pages/AccountSettings/Compilanc/COMreg/Comregistration.jsx
@@ -1,8 +1,116 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import "./Comregistration.css"
 const Comregistration = () => {
+  const staffsizes =[
+   {value: 'one', text: '1'},
+   {value:'two', text:"2"},
+  ];
+  const[elected, setElected] = useState('')
+  const[selected, setSelected] = useState(elected)
+  const [tbus, setTbus] = useState('');
+  const [street, setTstreet] = useState('');
+   const [apart, setTapart] = useState('');
+
+  // States for checking the errors
+  const [submitted, setSubmitted] = useState(false);
+  const [error, setError] = useState(false);
+ 
+const Tbus = (e) => {
+    setTbus(e.target.value);
+    setSubmitted(false);
+   }
+  
+   const Tstreet = (e) => {
+    setTstreet(e.target.value);
+    setSubmitted(false);
+   }
+    const Tapart = (e) => {
+    setTapart(e.target.value);
+    setSubmitted(false);
+    }
+    const staffChange = event =>{
+      console.log(event.target.value);
+      setSelected(event.target.value);
+    }
+    const handleSubmit = (e) => {
+    e.preventDefault();
+    if (selected === '' || Tapart=== '') {
+      setError(true);
+    } else {
+      setSubmitted(true);
+      setError(false);
+    }
+  }
+      const successMessage = () => {
+    return (
+      <div
+        className="success"
+        style={{
+          display: submitted ? '' : 'none',
+        }}>
+        <h1>User {} successfully registered!!</h1>
+      </div>
+    );
+    }
+
+    const errorMessage = () => {
+    return (
+      <div
+        className="error"
+        style={{
+          display: error ? '' : 'none',
+        }}>
+        <h1>Please enter all the fields</h1>
+      </div>
+    );
+  };
+  
   return (
-    <div>Comregistration</div>
+ 
+    <div className='Rainn'>
+    <div className='mporm'>
+    <h1 className='dress'>Registration</h1>
+    {/* Calling to the methods */}
+      
+        {errorMessage()}
+        {successMessage()}
+    <form className='style-form'>
+        {/* Labels and inputs for form data */}
+        {/*staff size*/}
+       <select className='sinput' value={selected} onChange={staffChange}>
+       <option  className="leo" value={elected} selected disabled >Select State or Region</option>
+    {staffsizes.map(staffsize =>(<option key={staffsize.value} value={staffsize.value} >{staffsize.text}</option>))}
+</select>
+{/*industry*/}
+       <select className='sinput' value={selected} onChange={staffChange}>
+       <option value={elected} selected disabled >City</option>
+    {staffsizes.map(staffsize =>(<option key={staffsize.value} value={staffsize.value} >{staffsize.text}</option>))}
+</select>
+     
+        <h2 className='address'>Registered Address</h2>
+              <input onChange={Tbus} className="input"
+          value={tbus} type="text" placeholder='Legal Business name'/>
+ <select className='sinput' value={selected} onChange={staffChange}>
+       <option value={elected} selected disabled >Select Registration type</option>
+    {staffsizes.map(staffsize =>(<option key={staffsize.value} value={staffsize.value} >{staffsize.text}</option>))}
+</select>
+
+    
+ <input onChange={Tstreet} className="input"
+          value={street} type="text" placeholder='Street Address'/>
+
+          <input onChange={Tapart} className="input"
+          value={apart} type="text" placeholder='Apartment or House number'/>
+
+
+          
+    </form>
+    </div>
+
+     <button onClick={handleSubmit} className="patn" type="submit">
+          Save changes
+        </button>
+    </div>
   )
 }
 
