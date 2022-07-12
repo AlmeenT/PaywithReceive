6a576394bf5066d982beab016a6fa2a93accf74f
AlmeenT <100124797+AlmeenT@users.noreply.github.com>
2022-07-11 23:50:05 -0700

    main nav respo

--- src/Components/Main Nav/MainNav.jsx
+++ src/Components/Main Nav/MainNav.jsx
@@ -8,9 +8,9 @@ const MainNav = () => {
     <div className='flex p-1 lg:pb-3 bg-white'>
         <Link to='/'><div className='flex ml-2 mt-4 lg:ml-24 lg:mt-3' >
             <img alt='' src={Logo} className='w-5 h-5 lg:w-10 lg:h-10' />
-            <h2 className='-mt-5 text-xs lg:text-2xl lg:-mt-0.5  text-orange-400'>re</h2><h2 className='-mt-5 text-xs lg:text-2xl lg:-mt-0.5 text-blue-800'>ceive</h2>
+            <h2 className='-mt-2 text-xs lg:text-2xl lg:-mt-0.5  text-orange-400'>re</h2><h2 className='-mt-2 text-xs lg:text-2xl lg:-mt-0.5 text-blue-800'>ceive</h2>
         </div></Link>
-        <div className='links flex ml-14 space-x-4 mt-1 lg:space-x-8 lg:mt-6 lg:ml-80'>
+        <div className='links flex ml-14 space-x-4 mt-3 lg:space-x-8 lg:mt-6 lg:ml-80'>
             <h2 className=' text-blue-800'>Developers</h2>
             <Link to='/about'><h2 className=' text-blue-800'>About us</h2></Link>
             <Link to='/support'><h2 className=' text-blue-800'>Support</h2></Link>
