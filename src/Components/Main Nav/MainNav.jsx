cce27c2ab29e828ddcaebfa7e4c15391188a0b8f
AlmeenT <100124797+AlmeenT@users.noreply.github.com>
2022-07-13 22:46:46 -0700

    ok1

--- src/Components/Main Nav/MainNav.jsx
+++ src/Components/Main Nav/MainNav.jsx
@@ -5,12 +5,12 @@ import './MainNav.css'
 
 const MainNav = () => {
   return (
-    <div className='flex pb-0.5 lg:pb-1 bg-white'>
+    <div className='flex lg:pb-1 bg-white'>
         <Link to='/'><div className='flex ml-2 mt-4 lg:ml-24 lg:mt-5' >
             <img alt='' src={Logo} className='w-5 h-5 lg:w-10 lg:h-10' />
-            <h2 className='-mt-5 text-xs lg:text-2xl lg:-mt-5  text-orange-400'>re</h2><h2 className='-mt-5 text-xs lg:text-2xl lg:-mt-5 text-blue-800'>ceive</h2>
+            <h2 className='-mt-6 text-xs lg:text-2xl lg:-mt-5  text-orange-400'>re</h2><h2 className='-mt-5 text-xs lg:text-2xl lg:-mt-5 text-blue-800'>ceive</h2>
         </div></Link>
-        <div className='links flex ml-14 space-x-4 mt-3 lg:space-x-8 lg:mt-3 lg:ml-80'>
+        <div className='links flex ml-14 space-x-4 mt-2 lg:space-x-8 lg:mt-3 lg:ml-80'>
             <h2 className=' text-blue-800'>Developers</h2>
             <Link to='/about'><h2 className=' text-blue-800'>About us</h2></Link>
             <Link to='/support'><h2 className=' text-blue-800'>Support</h2></Link>
